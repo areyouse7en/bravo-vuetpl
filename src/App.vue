62276e087e2820a3a38de1df8ab189efe5760079
seven <373084212@qq.com>
2018-03-06 23:26:55 +0800

    feat:htttp

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,15 @@
 <template lang="pug">
   #app Bravo!
 </template>
+
+<script>
+export default {
+  async mounted() {
+    const { data } = await this.$http.get(
+      "http://jsonplaceholder.typicode.com/posts",
+      ""
+    );
+    console.log(data);
+  }
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import http from 'bravo-http'
+Vue.prototype.$http = http
+
 Vue.config.productionTip = false
 
 new Vue({
