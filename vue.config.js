c30de3f2a32c6adc696afc71cf15c4b385a954c3
seven <373084212@qq.com>
2018-05-04 15:29:10 +0800

    dep: update

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  devServer: {
+    // open: process.platform === 'darwin',
+    host: '0.0.0.0',
+    port: 8001,
+    // https: false,
+    // hotOnly: false,
+    proxy: {
+      '/api': {
+        target: "http://ruizhu.nicknideveloper.net",
+        pathRewrite: {
+          "^/api": "/ruizhu-erp-api"
+        },
+        ws: true,
+        changeOrigin: true
+      }
+    }
+  }
+}
